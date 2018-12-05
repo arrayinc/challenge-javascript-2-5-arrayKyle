345ac025e18a936da0db5389f205cebd0c5e6de6
Kyle Hong <kylehong@rocketmail.com>
2018-12-05 16:25:24 -0700

    challenge 5!!!!!!!!!!!!!!!!!

--- answer.js
+++ answer.js
@@ -1,1 +1,14 @@
 // *** YOUR ANSWER BELOW ***
+function negate(x){
+let negate="un"+x;
+return negate;
+}
+function intensify(x){
+let intensify="plus"+x;
+return intensify
+}
+function reinforce(x){
+let reinforce="double"+x;
+return reinforce
+}
+reinforce(intensify(negate("good")));
